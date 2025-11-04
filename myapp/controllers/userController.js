0e3b81534ed686860a78db621b613c82dd0682b5
MartinaSciarrotta <gsciarrotta@udesa.edu.ar>
2025-11-04 17:34:09 -0300

    corregir userController

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -2,31 +2,64 @@ const db = require("../database/models");
 const op = db.Sequelize.Op;
 const bcrypt = require('bcryptjs');
 
-const userController = {
-    //login: 
+let userController = {
+    show: function(req, res) {
+      
+            return res.render("register");     
+    },
 
-    //processLogin:
+    create: function(req, res){
+        let email = req.body.email;
+        let contrasena = req.body.contrasena;
+        let usuario = req.body.usuario;
 
+        // guardar el usuario
+        let infoUsuario = {
+            usuario: usuario,
+            email: email,
+            contrasena: bcryptjs.hashSync(contrasena, 10)
+        }
 
-    logout: function (req, res) {
-       req.session.destroy();
+        db.User.create(usuario)
+            .then(function(resultados) {
+                return res.redirect("/", {infoUsuario: resultados})
+            })
+            .catch(function(err) {
+                return res.send(err)
+            })
+
+    },
+
+    login: function(req, res){
+       
+        return res.render('login');
+    },
+
+    processLogin: function(req,res){
+        let userInfo = {
+            email: req.body.email,
+            password: req.body.password,
+            recordarme: req.body.recordarme,
+        }
   
-       if (req.cookies.recordarme) {
-           res.clearCookie('recordarme');
-       }
-  
-       return res.redirect('/'); 
-   },
+        req.session.user = userInfo;
 
-   //register:
+        //checkear si recordarme esta tildado
 
-   //create:
+        if (userInfo.recordarme != undefined) {
+            res.cookie("user", userInfo, {maxAge: 600000}) // En milisegundos
+        }
 
+        res.redirect ("/")
+    },
 
+    logout: function(req,res){
+        req.session.destroy();
+        res.clearCookie("userData");
 
+        return res.redirect('/')
+    }
+  };
 
-
-
-}
 
 module.exports = userController;
