7923de8a1670e51252c4b13e02c625d39338d7a6
KailaniFarkas <kfarkas@udesa.edu.ar>
2025-11-06 14:13:44 -0300

    detalle

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -2,25 +2,38 @@ const db = require("../database/models");
 const op = db.Sequelize.Op;
 
 const productController = {
-    productDetail: function (req, res) {
-        let id = req.params.id; 
-        let autoEncontrado = null;
-    
-        for (let i = 0; i < db.productos.length; i++) {
-          if (db.productos[i].id == id) {
-            autoEncontrado = db.productos[i];
-            break;
-          }
+  productDetail: function (req, res) {
+    let id = req.params.id;
+
+    let relaciones = {
+      include: [
+        { association: "usuario" },
+        { association: "comentarios", include: [{ association: "usuario" }] }
+      ],
+      where: { id: id }
+    };
+
+    db.Producto.findByPk(id, relaciones)
+      .then(function (product) {
+        if (!product) {
+          return res.send("Producto no encontrado" );
         }
-    
-        return res.render("product", { 
-            product: autoEncontrado
-        });
-    },
+        return res.render("product", { product: product });
+      })
+
+      .catch(function (error) {
+        console.error(error);
+      });
+  },
 
 
     productAdd: function(req, res) {
-        res.render('product-add');
+      // Verificar si el usuario está logueado
+      if (!req.session.usuariooo) {  // Asumiendo que 'usuariooo' es el objeto de sesión del usuario
+        return res.redirect('/login');  // Redirigir a la página de login si no está logueado
+      }
+
+      res.render('product-add');
     },
 
     productEdit: function(req, res) {
